b873b849d9aeef2b17f3d87648e7c578505ef0bb
carlosrehu <crendon@rollins.edu>
2017-04-27 17:36:31 +0000

    Sync

--- app/views.py
+++ app/views.py
@@ -424,7 +424,7 @@ def postaquestion():
         
         #answertest = cur.execute(""" SELECT question FROM answers LEFT JOIN questions ON answers.answer_id = question_id  """)
         #answertest = cur.fetchall()
-##
+##test
         print cur.fetchall()
         print 'printing tests'
         #print questiontest
@@ -531,19 +531,3 @@ def typeofquestions():
 @application.route('/verbalquestions')
 def verbalquestions():
     return render_template('verbalquestions.html')
-
-@application.route('/underconstruction')
-def underconstruction():
-        if request.method == 'GET':
-            if 'username' in session:
-                username=session['username']
-
-                cur.execute("SELECT f_name, l_name, email, gender, major, minor, age, user_name, password, phone_number, profile_type, graduate_date, profession, about_me, interests, skills, city, state FROM user WHERE user_name = %s", [username])
-
-                data = cur.fetchone()
-                print data
-                print cur.fetchone()
-                return render_template('underconstruction.html', items=data)
-
-        return redirect(url_for('signin'))
-        return render_template('underconstruction.html')
